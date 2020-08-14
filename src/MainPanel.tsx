36f773ec2f38a35b817ff87a9a73781a40488c23
Nam Le <nam.leduc02@gmail.com>
2020-08-14 13:15:40 +0200

    1st commit

---
+++ src/MainPanel.tsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import { PanelProps } from '@grafana/data';
+import { PanelOptions } from 'types';
+
+interface Props extends PanelProps<PanelOptions> {}
+interface State {}
+
+export class MainPanel extends PureComponent<Props, State> {
+  componentDidMount() {
+    console.log('timelines ', this.props.data);
+  }
+
+  componentDidUpdate(prevProps: Props) {}
+
+  render() {
+    const { width, height } = this.props;
+
+    return (
+      <div
+        style={{
+          width,
+          height,
+          padding: 10,
+        }}
+      >
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
